dcb4d6fbaf9a5a593c3277f15a1bba5db0ef4cfe
myu8358 <myu8358@yahoo.co.jp>
2016-02-10 08:03:56 +0000

    user-profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # プロフィールは400文字以内
   validates :profile , length: { maximum: 400 }
   # 地域は2文字以上30文字以下
-  validates :region , length: { maximum: 30 }
+  validates :region , length: { maximum: 30 }, on: :update
   
     before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
