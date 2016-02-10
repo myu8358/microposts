eff87df3b9ff27fbb989833f4a955db22f90a4e1
myu8358 <myu8358@yahoo.co.jp>
2016-02-10 03:15:28 +0000

    user-profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
+  # プロフィールは400文字以内
+  validates :profile , length: { maximum: 400 }
+  # 地域は2文字以上30文字以下
+  validates :region , length: { minimum: 2, maximum: 30 }
+  
     before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
