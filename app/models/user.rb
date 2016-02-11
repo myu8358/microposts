2706f07d3d3505eb0d7317964b461a5e123e86c8
myu8358 <myu8358@yahoo.co.jp>
2016-02-11 20:29:41 +0000

    followings-followers

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # プロフィールは400文字以内
   validates :profile , length: { maximum: 400 }
   # 地域は100文字以下
-  validates :region, presence: true, length: { maximum: 100 }, on: :update
+  validates :region, presence: true, length: { maximum: 30 }, on: :update
   
     before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
